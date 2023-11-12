5bd6a81a9386aa0f18ec6200081ecb7428ad1226
Dmitriy Volkov <d1mav0lk0v@yandex.ru>
2023-11-12 14:02:16 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,118 @@
+# Split file
+
+### Info
+
+Split one file into several files given command line arguments.
+
+Arguments:
+* ```-h``` show this help message and exit;
+* one option to select, title line does not count:
+  * ```-l n``` create output files with **n** lines, the last file may contain fewer lines;
+  * ```-f n``` create **n** files, the difference in the number of lines in these files is no more than one;
+* ```-e``` encoding of source file and output files;
+* ```-t``` include title (first line of the source file) in output files;
+* ```-v``` display created output files;
+* ```source_file``` file name or full path to the file that will be split;
+* ```target_dir ``` directory of output files (default: dir source_file), the files will have the same name, extension, encoding, but a new numbering suffix.
+
+---
+
+### Prerequisites
+
+Language version starting from Python 3.10+
+
+```bash
+$ python --version
+Python 3.10.4
+```
+
+---
+
+### Usage
+
+```bash
+$ python split_file.py -h
+usage: split_file.py [-h] (-l NLINES | -f NFILES) [-e ENCODING] [-t] [-v] source_file [target_dir]
+
+description:
+    Split one file into several files given command line arguments.
+    New files are created with a sequence number suffix.
+    Warning: new files may erase old files!
+
+positional arguments:
+  source_file           file name or full path file
+  target_dir            directory of output files (default: dir source_file)
+
+options:
+  -h, --help            show this help message and exit
+  -l NLINES, --nlines NLINES
+                        number of lines in output files
+  -f NFILES, --nfiles NFILES
+                        number of output files
+  -e ENCODING, --encoding ENCODING
+                        encoding of source file and output files
+  -t, --title           include title (first line of the source file) in output files
+  -v, --verbose         display created output files
+```
+
+```bash
+$ cat temp/input.csv
+0
+1
+2
+3
+4
+5
+6
+```
+
+```bash
+$ python split_file.py -l 4 -t -v temp/input.csv
+start...
+temp\input_1.csv
+temp\input_2.csv
+success!
+
+$ cat temp/input_1.csv
+0
+1
+2
+3
+4
+
+$ cat temp/input_2.csv
+0
+5
+6
+```
+
+```bash
+$ python split_file.py -f 3 -v temp/input.csv
+start...
+temp\input_1.csv
+temp\input_2.csv
+temp\input_3.csv
+temp\input_4.csv
+success!
+
+$ cat temp/input_1.csv
+0
+1
+2
+
+$ cat temp/input_2.csv
+3
+4
+
+$ cat temp/input_3.csv
+5
+6
+```
+
+---
+
+### Authors
+
+* [Dmitry Volkov](https://github.com/d1mav0lk0v)
+
+---
---
+++ split_file.py
@@ -0,0 +1,236 @@
+"""Split one file into several files given command line arguments."""
+
+
+import os
+import sys
+import argparse
+
+from itertools import count
+
+
+RED     = "\033[31m"
+GREEN   = "\033[32m"
+BLUE    = "\033[34m"
+YELLOW  = "\033[33m"
+RESET   = "\033[0m"
+
+
+def error(message):
+    """Show an error message and exit the program."""
+
+    print(RED + "error: {}".format(message) + RESET)
+    sys.exit(1)
+
+
+def positive_int(value):
+    """Validate a string for positive number value and returns that number."""
+
+    try:
+        value = int(value)
+        if value <= 0:
+            raise ValueError
+    except ValueError:
+        raise argparse.ArgumentTypeError(
+            "not a positive integer: {}".format(value)
+        )
+
+    return value
+
+
+def parse_command_args():
+    """Run the command line arguments parser."""
+
+    description = """description:
+    {0}Split one file into several files given command line arguments.
+    {0}New files are created with a sequence number suffix.
+    {1}Warning: new files may erase old files!{2}""".format(GREEN, YELLOW, RESET)
+
+    parser = argparse.ArgumentParser(
+        description=description,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    parser.error = error
+
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    group.add_argument(
+        "-l", "--nlines",
+        type=positive_int,
+        help="number of lines in output files",
+    )
+
+    group.add_argument(
+        "-f", "--nfiles",
+        type=positive_int,
+        help="number of output files",
+    )
+
+    parser.add_argument(
+        "-e", "--encoding",
+        help="encoding of source file and output files"
+    )
+
+    parser.add_argument(
+        "-t", "--title",
+        action="store_true",
+        help="include title (first line of the source file) in output files",
+    )
+
+    parser.add_argument(
+        "-v", "--verbose",
+        action="store_true",
+        help="display created output files",
+    )
+
+    parser.add_argument(
+        "source_file",
+        help="file name or full path file",
+    )
+
+    parser.add_argument(
+        "target_dir",
+        nargs="?",
+        default=None,
+        help="directory of output files (default: dir source_file)",
+    )
+
+    return parser.parse_args()
+
+
+def get_target_file_format(source_name, target_dir=None):
+    """Return the format string in the file suffix.
+    The string is the path to the file."""
+
+    path, file = os.path.split(source_name)
+    name, ext = os.path.splitext(file)
+    if target_dir is not None:
+        path = target_dir
+
+    return os.path.join(path, name + "_{}" + ext)
+
+
+def del_newline_endfile(file_name):
+    """Remove new line at end of file."""
+
+    with open(file_name, "rb+") as file:
+        file.seek(0, 2)
+        size = max(0, file.tell() - 2)
+
+        file.seek(size, 0)
+        end = file.read()
+
+        size_diff = len(end) - len(end.rstrip())
+        if size_diff != 0:
+            file.truncate(file.tell() - size_diff)
+
+
+def split_file_nlines(source_name, nlines, target_dir=None,
+                      encoding=None, title=False, verbose=False):
+    """Split source file by number of lines.
+
+    The number of lines in the new files is the same, maybe except for the
+    last one. If the total number of lines is not a multiple of the given
+    number, then the last file will have fewer lines.
+    """
+
+    target_name_format = get_target_file_format(source_name, target_dir)
+
+    with open(source_name, mode="r", encoding=encoding) as source:
+        title_line = source.readline() if title else ""
+
+        break_loop = False
+        for i in count(1):
+            target_name = target_name_format.format(i)
+            if verbose:
+                print(BLUE + target_name + RESET)
+
+            with open(target_name, mode="w+", encoding=encoding) as target:
+                target.write(title_line)
+
+                for _ in range(nlines):
+                    line = source.readline()
+                    target.write(line)
+                    if not line:
+                        break_loop = True
+                        break
+
+            del_newline_endfile(target_name)
+
+            if break_loop:
+                break
+
+
+def split_file_nfiles(source_name, nfiles, target_dir=None,
+                      encoding=None, title=False, verbose=False):
+    """Split source file by number of files.
+
+    The resulting files contain approximately the same number of lines.
+    The biggest difference is one line.
+    """
+
+    target_name_format = get_target_file_format(source_name, target_dir)
+
+    with open(source_name, mode="r", encoding=encoding) as source:
+        source_nlines = (-1 if title else 0) + sum(1 for _ in source) # O(n)
+        source.seek(0, 0)
+
+        quot_source_nlines, rest_source_nlines = divmod(source_nlines, nfiles)
+
+        title_line = source.readline() if title else ""
+
+        for i in range(1, nfiles + 1):
+            target_name = target_name_format.format(i)
+            if verbose:
+                print(BLUE + target_name + RESET)
+
+            nlines = quot_source_nlines
+            if i <= rest_source_nlines:
+                nlines += 1
+
+            with open(target_name, mode="w", encoding=encoding) as target:
+                target.write(title_line)
+
+                for _ in range(nlines):
+                    line = source.readline()
+                    target.write(line)
+
+            del_newline_endfile(target_name)
+
+
+def main():
+    """Run functions: parser command args and split file."""
+
+    args = parse_command_args()
+
+    if args.verbose:
+        print(GREEN + "start..."  + RESET)
+
+    try:
+        if args.nlines is not None:
+            split_file_nlines(
+                args.source_file,
+                args.nlines,
+                args.target_dir,
+                args.encoding,
+                args.title,
+                args.verbose,
+            )
+        elif args.nfiles is not None:
+            split_file_nfiles(
+                args.source_file,
+                args.nfiles,
+                args.target_dir,
+                args.encoding,
+                args.title,
+                args.verbose,
+            )
+    except Exception as exc:
+        error(exc)
+    else:
+        if args.verbose:
+            print(GREEN + "success!"  + RESET)
+
+
+if __name__ == "__main__":
+    main()
